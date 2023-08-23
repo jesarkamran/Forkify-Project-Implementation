47aa1396f17f784ed38e37b317d6915dbe8575ef
jesarkamran <jesarkamran@gmail.com>
2023-08-23 11:35:18 +0500

    Added All files related to MVC Architecture Implementation

--- src/js/controller.js
+++ src/js/controller.js
@@ -29,9 +29,13 @@ const renderSpinner = function (parentEL) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     renderSpinner(recipeContainer);
     const resp = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await resp.json();
@@ -49,7 +53,6 @@ const showRecipe = async function () {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(recipe);
 
     const markup = `
       <figure class="recipe__fig">
@@ -156,4 +159,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
